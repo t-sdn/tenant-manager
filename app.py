569b03b58f28cb20cdcd87a4b117b23f7cdb1e03
kjwon15 <kjwonmail@gmail.com>
2015-08-27 09:55:24 +0900

    Make VLAN tab

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, request
-from scripts import make_tenant
+from scripts import make_tenant_vlan, make_tenant_vxlan
 
 
 app = Flask(__name__)
@@ -13,16 +13,28 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/create/<name>', methods=['POST'])
-@app.route('/create/', defaults={'name': None}, methods=['POST'])
-def create_tenant(name):
+@app.route('/vlan/create/<name>', methods=['POST'])
+@app.route('/vlan/create/', defaults={'name': None}, methods=['POST'])
+def create_tenant_vlan(name):
+    name = name if name else request.form['name']
+    ip = request.form['ip']
+    vid = request.form['vid']
+    interface = app.config['INTERFACE']
+
+    stdout, stderr = make_tenant_vlan(name, ip, vid, interface)
+    return render_template('result.html', stdout=stdout, stderr=stderr)
+
+
+@app.route('/vxlan/create/<name>', methods=['POST'])
+@app.route('/vxlan/create/', defaults={'name': None}, methods=['POST'])
+def create_tenant_vxlan(name):
     name = name if name else request.form['name']
     ip = request.form['ip']
     vni = request.form['vni']
     interface = app.config['INTERFACE']
-    stdout, stderr = make_tenant(name, ip, vni, interface)
 
-    return render_template('error.html', stdout=stdout, stderr=stderr)
+    stdout, stderr = make_tenant_vxlan(name, ip, vni, interface)
+    return render_template('result.html', stdout=stdout, stderr=stderr)
 
 
 if __name__ == '__main__':
--- scripts.py
+++ scripts.py
@@ -18,7 +18,54 @@ def run_script(script):
     return stdout, stderr
 
 
-def make_tenant(name, ip, vni, interface):
+def make_tenant_vlan(name, ip, vid, interface):
+    """
+    Make tenant network with vlan
+    :param name: tenant name.
+    :param ip: IP address assigned to tenant network.
+    :param vid: VID number.
+    :param interface: Make tenant network using this interface.
+    """
+
+    script = '\n'.join([
+        'name={}',
+        'ip={}',
+        'vid={}',
+        'interface={}',
+        '',
+        '#',
+        '# Binding br_ext to $interface',
+        '#',
+        'sudo brctl addbr br_ext',
+        'sudo ip link set dev br_ext up',
+        'sudo brctl addif br_ext $interface',
+        '',
+        '#',
+        '# Creating a namespace with $name with $ip',
+        '# ',
+        '',
+        'sudo ip netns add $name',
+        'sudo brctl addbr br_$name',
+        'sudo ip link set dev br_$name up',
+        'sudo ip link add veth0 type veth peer name veth0_$name ',
+        'sudo ip link set veth0 netns $name',
+        'sudo ip netns exec $name ip link set dev veth0 up',
+        'sudo ip netns exec $name ifconfig veth0 $ip netmask 255.255.255.0 up',
+        'sudo ip link set dev veth0_$name up',
+        '',
+        '#',
+        '# Binding VID $vid to br_$name',
+        '# Binding veth0_$name to br_$name',
+        '#',
+        'sudo ip link add link br_ext br_ext.$vid type vlan id $vid',
+        'sudo ip link set dev br_ext.$vid up',
+        'sudo brctl addif br_$name veth0_$name',
+        'sudo brctl addif br_$name br_ext.$vid',
+    ]).format(name, ip, vid, interface)
+    return run_script(script)
+
+
+def make_tenant_vxlan(name, ip, vni, interface):
     """
     Make tenant network
 
